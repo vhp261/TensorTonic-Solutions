864c646af5b071a7785909243a98a6cf3e62ee80
vhp261 <pvh2617@gmail.com>
2026-08-11 12:04:46 +0700

    Solve cosine-annealing-lr

---
+++ cosine-annealing-lr/cosine-annealing-lr.py
@@ -0,0 +1,7 @@
+import numpy as np
+def cosine_annealing_schedule(base_lr, min_lr, total_steps, current_step):
+    """
+    Compute the learning rate using cosine annealing.
+    """
+    # Write code here
+    return min_lr+1/2*(base_lr-min_lr)*(1+np.cos(np.pi*current_step/total_steps))
